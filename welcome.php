943e1e69b708b1c6ebcccbab3c0d04dfc6bec973
MandeepDalavi <mandeepdalavi@gmail.com>
2021-06-15 19:12:05 +0530

    Added anim CSS for Welcome.php

--- welcome.php
+++ welcome.php
@@ -9,6 +9,8 @@
     <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
     <link rel="manifest" href="img/favicon/site.webmanifest">
+    <!-- anim css -->
+    <link rel="stylesheet" type="text/css" href="css/anim.css">
     <title>Welcome</title>
 </head>
 <body>
