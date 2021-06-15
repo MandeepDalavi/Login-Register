77c21f80d86d32be7a5b51ebbb8ca7b3a8f29216
MandeepDalavi <mandeepdalavi@gmail.com>
2021-06-15 18:32:39 +0530

    Basic HTML Login Structure ready

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-IN">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- main css -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <form class="login-email">
+            <p class="login-text">Login with Email</p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" name="email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" name="password" required>
+            </div>
+            <div class="input-group">
+                <button class="btn" name="submit">Login</button>
+            </div>
+            <p class="login-register-text">Don't have an account?<a href="register.php">Register Here</a></p>
+        </form>
+    </div>
+</body>
+</html>
