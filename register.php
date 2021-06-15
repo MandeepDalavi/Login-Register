57e5a4cf242ba9000c5a7c7e236eb6c212670210
MandeepDalavi <mandeepdalavi@gmail.com>
2021-06-15 19:34:47 +0530

    Register Database Connection

--- register.php
+++ register.php
@@ -1,3 +1,45 @@
+<?php
+    include 'config.php';
+
+    error_reporting(0);
+
+    session_start();
+
+    if(isset($_SESSION['username'])) {
+        header("Location: login.php");
+    }
+
+    if(isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+        $cpassword = md5($_POST['cpassword']);
+
+        if($password == $cpassword) {
+            $sql = "SELECT * FROM test WHERE email='$email'";
+            $result = mysqli_query($connection, $sql);
+            if(!$result->num_rows > 0) {
+                $sql = "INSERT INTO test (username, email, password)
+                        VALUES ('$username', '$email', '$password')";
+                $result = mysqli_query($connection, $sql);
+                if($result) {
+                    echo "<script>alert('Wow! User Registered Successfully.')</script>";
+                    $username = "";
+                    $email = "";
+                    $_POST['password'] = "";
+                    $_POST['cpassword'] = "";
+                } else {
+                    echo "<script>alert('Woops! Something went wrong.')</script>";
+                }
+            } else {
+                echo "<script>alert('Email already exists!')</script>";
+            }
+        } else {
+            echo "<script>alert('Password Not Matched!')</script>";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en-IN">
 <head>
@@ -17,19 +59,19 @@
 </head>
 <body>
     <div class="container">
-        <form class="login-email">
+        <form action="" method="POST" class="login-email">
             <p class="login-text">Register</p>
             <div class="input-group">
-                <input type="text" placeholder="Username" name="username" required>
+                <input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
             </div>
             <div class="input-group">
-                <input type="email" placeholder="Email" name="email" required>
+                <input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Password" name="password" required>
+                <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Confirm Password" name="cpassword" required>
+                <input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
             </div>
             <div class="input-group">
                 <button class="btn" name="submit">Register</button>
