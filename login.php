9e621464f393911e624390370f08d1b25fafaeb0
MandeepDalavi <mandeepdalavi@gmail.com>
2021-06-15 18:47:40 +0530

    Added Font Awesome

--- login.php
+++ login.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- main css -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Login</title>
